baff3d7354ed57ada1ebaacb4863dffed68c9d20
DevjyotiDey <devjyoti.dey@verizon.com>
2015-09-29 21:52:17 +0530

    VO commit

---
+++ src/com/verizon/vo/Interviewee.java
@@ -0,0 +1,55 @@
+package com.verizon.vo;
+
+public class Interviewee {
+	private String address;
+	private int employeeID;
+	private int interviewId;
+	private int intervieweeId;
+	private String intervieweeName;
+	private String intervieweeOrg;
+	private String mobileNumber;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public int getInterviewId() {
+		return interviewId;
+	}
+	public void setInterviewId(int interviewId) {
+		this.interviewId = interviewId;
+	}
+	public int getIntervieweeId() {
+		return intervieweeId;
+	}
+	public void setIntervieweeId(int intervieweeId) {
+		this.intervieweeId = intervieweeId;
+	}
+	public String getIntervieweeName() {
+		return intervieweeName;
+	}
+	public void setIntervieweeName(String intervieweeName) {
+		this.intervieweeName = intervieweeName;
+	}
+	public String getIntervieweeOrg() {
+		return intervieweeOrg;
+	}
+	public void setIntervieweeOrg(String intervieweeOrg) {
+		this.intervieweeOrg = intervieweeOrg;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+}
