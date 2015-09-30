8f531f0eef18f42b561cce7734028141b7ecf10e
DevjyotiDey <devjyoti.dey@verizon.com>
2015-09-30 22:33:04 +0530

    reloading records

--- src/com/verizon/loginServlet.java
+++ src/com/verizon/loginServlet.java
@@ -19,6 +19,9 @@ public class loginServlet extends HttpServlet
     {
         super();
     }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	doPost(request,response);
+    }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
