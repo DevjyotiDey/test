18f390732b3674b907a8f890369659b3ea9b8854
DevjyotiDey <devjyoti.dey@verizon.com>
2015-09-30 11:26:41 +0530

    DButils

--- src/com/verizon/utils/DBUtis.java
+++ src/com/verizon/utils/DBUtis.java
@@ -1,6 +1,8 @@
 package com.verizon.utils;
 import java.sql.*;
 
+import com.verizon.vo.*;
+
 public class DBUtis {
 
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
@@ -36,5 +38,124 @@ public class DBUtis {
 			e.printStackTrace();
 		}
     }
+    
+    public static boolean insertAll(InterviewDetail interviewDtl,Interviewee interviewee,Interviewer interviewer){
+    	boolean retVal=false;
+    	
+		Connection conn=DBUtis.getConnection();
+		if(conn!=null){
+			try {
+				retVal=createInterview(interviewDtl,conn) &&
+				insertInterviewer(interviewer, conn) &&
+				insertInterviewee(interviewee, conn);
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				closeConnection(conn);
+			}
+		}
+    	
+    	return retVal;
+    }
+    
+ 
+    //1.
+    public static boolean createInterview(InterviewDetail interview,Connection conn){
+    	boolean retVal=false;
+    	
+    	final String CREATE_INTERVIEW="insert into interview(INTERVIEW_DATE, INTERVIEW_TYPE, ADDL_DOCUMENT, RESULT, FEEDBACK, VENUE, EVALUATION_RESULT) values (?,?,?,?,?,?,?)";
+    	
+    	if(interview==null){
+    		return retVal;
+    	}
+    	
+
+		if(conn!=null){
+			try {
+				PreparedStatement ps=conn.prepareStatement(CREATE_INTERVIEW);
+				ps.setDate(1,interview.getInterviewDate());
+				ps.setString(2,interview.getInterviewType());
+				ps.setBlob(3, interview.getAddlDocument());
+				ps.setString(4, interview.getResult());
+				ps.setString(5,interview.getFeedback());
+				ps.setString(6,interview.getVenue());
+				ps.setString(7, interview.getEvaluationResults());
+				
+				ps.execute();
+				retVal=true;
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				//closeConnection(conn);
+			}
+		}
+    	
+    	return retVal;
+    }
+    
+    
+   // 2.
+    public static boolean insertInterviewee(Interviewee interviewee,Connection conn){
+    	boolean returnVal=false;
+    	final String INSERT_INTERVIEWEE="insert into interviewee(INTERVIEWEE_NAME, INTERVIEWEE_ORG, EMPLOYEE_ID, ADDRESS, MOBILE_NO, INTERVIEW_ID)"
+    			+ " values (?,?,?,?,?,?)";
+    	
+    	if(interviewee==null){
+    		return returnVal;
+    	}
+    	
+		
+		if(conn!=null){
+			try {
+				PreparedStatement ps=conn.prepareStatement(INSERT_INTERVIEWEE);
+				ps.setString(1,interviewee.getIntervieweeName());
+				ps.setString(2,interviewee.getIntervieweeOrg());
+				ps.setInt(3, interviewee.getEmployeeID());
+				ps.setString(4, interviewee.getAddress());
+				ps.setString(5,interviewee.getMobileNumber());
+				ps.setInt(6,getLastInsertId(conn));
+				
+				ps.execute();
+				returnVal=true;
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				//closeConnection(conn);
+			}
+		}
+    	return returnVal;
+    }
 	
+    //3.
+    public static boolean insertInterviewer(Interviewer interviewer,Connection conn){
+    	boolean retVal=false;
+		if(conn!=null){
+			try {
+				PreparedStatement ps=conn.prepareStatement("INSERT INTO INTERVIEWER VALUES(?,?)");
+				ps.setInt(1, getLastInsertId(conn));
+				ps.setInt(2, interviewer.getInterviewerEmpId());
+				ps.execute();
+				retVal=true;
+			}catch(Exception e){
+				
+			}
+		}
+		return retVal;
+    }
+    
+    public static int getLastInsertId(Connection conn){
+    	int returnVal=0;
+    	if(conn!=null){
+			try {
+				PreparedStatement ps=conn.prepareStatement("select LAST_INSERT_ID()");
+				ResultSet rs = ps.executeQuery();
+				if(rs!=null && rs.next()){
+					returnVal=rs.getInt(1);
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+    	}
+    	return returnVal;
+    }
 }
